8b981663be158567763b0c3e3bcc7ae6d167e696
PapitoJorgex <144342076+PapitoJorgex@users.noreply.github.com>
2023-11-21 21:02:02 +0000

    finish day 5

--- programa5.py
+++ programa5.py
@@ -19,14 +19,75 @@
 # print(f"average of students = {average_students}")
 
 
-#EJERCICIO 2
-student_score = input().split()
-for n in range(0, len(student_score)):
-    student_score[n] = int(student_score[n])
+# #EJERCICIO 2
+# student_score = input().split()
+# for n in range(0, len(student_score)):
+#     student_score[n] = int(student_score[n])
 
-highest_score = 0
-for score in student_score:
-    if score > highest_score:
-        highest_score = score
+# highest_score = 0
+# for score in student_score:
+#     if score > highest_score:
+#         highest_score = score
 
-print(f"the highest score {n} is {highest_score}")
+# print(f"the highest score {n} is {highest_score}")
+
+
+#Ejercicio 3
+# target = int(input())
+
+# even_sum = 0
+
+# for number in range(2, target + 1, 2):
+#     even_sum += number
+# print(even_sum)
+
+#Ejercicio 4
+
+# print("Vamos a jugar al FizzBuzz")
+# nm = int(input("¿Hasta que número quieres jugarlo?: "))
+
+# for numero in range(1, nm + 1):
+#     if numero % 3 == 0:
+#          print("Fizz")
+#     elif numero % 5 == 0:
+#          print("Buzz")
+#     else:
+#         print(numero)
+
+#Ejercicio 5
+
+import random
+from string import ascii_letters
+abc = ascii_letters
+letras = list(abc)
+letras.extend(['ñ', 'Ñ'])
+
+numeros = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+
+simbolos = ['!', '¡', '?', '¿', '*', '+', '/', '(', ')', '$']
+
+print("Vamos a generar tu contraseña")
+
+n_letras = int(input("¿Cuántas letras quieres que tenga la contraseña?: "))
+n_numeros = int(input("¿Cuántos números quieres que tenga la contraseña?: "))
+n_simbolos = int(input("¿Cuántos símbolos quieres que tenga la contraseña?: "))
+
+passw = []
+
+for char in range(1, n_letras + 1):
+    letra_random = random.choice(letras)
+    passw += letra_random
+
+for nm in range(1, n_numeros + 1):
+    num_random = random.choice(numeros)
+    passw += num_random
+
+for sim in range(1, n_simbolos + 1):
+    sim_random = random.choice(simbolos)
+    passw += sim_random
+
+random.shuffle(passw)
+
+final = "".join(passw)
+
+print(final)
