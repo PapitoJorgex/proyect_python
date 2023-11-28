e02704478db001a6c69c54996ee532e7a94d2023
PapitoJorgex <144342076+PapitoJorgex@users.noreply.github.com>
2023-11-28 20:37:38 +0000

    finish ahoracados

--- programa7.py
+++ programa7.py
@@ -1,27 +1,30 @@
 import random
 print("Vamos a jugar al ahorcados")
 casilla = []
+numero = 0
 lista_palabras = ['irene', 'jorge', 'saiko', 'rolly']
-
-palabra = random.choice(lista_palabras)
-
-palabra_final = list(palabra)
-
 final = False
+eleccion = random.choice(lista_palabras)
+palabra = list(eleccion)
+
+for _ in range(len(palabra)):
+    casilla += "_"
+print(casilla)
+print(" ")
 
 while not final:
-    user = input("Dime una letra: ").lower()
-    for n in palabra_final:
-        if user == n:
-             casilla += n
-        else:
-           casilla = n
-           casilla.replace(n, "_")
-           print(casilla)
-           
-        
-    # word = ' '.join(casilla)
-    # print(word)
-    # if "_" not in word:
-    #     final = True
-print("FIN")
+    user = input("Averigua la letra: ")
+    for posicion in range(len(palabra)):
+        letra = palabra[posicion]
+        if letra == user:
+            casilla[posicion] = letra
+    relleno = " ".join(casilla)
+    print(relleno)
+    if user not in palabra:
+        numero += 1
+        if numero == 5:
+            print("Perdiste")
+            final = True
+    if "_" not in casilla:
+     final = True
+     print("Ganaste")
